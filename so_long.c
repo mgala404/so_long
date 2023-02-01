85726567061d630ca413b50d405763bdad7dec78
Lorenzo Lombardi <llombard@c1r1p9.42firenze.it>
2023-02-01 17:39:37 +0100

    asegsgsd

--- so_long.c
+++ so_long.c
@@ -43,7 +43,25 @@ char    *getmap(char *path)
     return (onzo);
 }
 
-void    map_cord(t_vb *tat, char *path)
+char **mapmat(char *rawmap)
+{
+    char **map;
+
+    map = ft_split(rawmap, '\n');
+    return(map);
+}
+
+int     ft_matlen(char **map)
+{
+    int y;
+
+    y = 0;
+    while(map[y])
+        y++;
+    return (y);
+}
+
+/*void    map_cord(t_vb *tat, char *path)
 {
     int     i;
     int     y;
@@ -65,40 +83,8 @@ void    map_cord(t_vb *tat, char *path)
     tat->mappa = (t_pos **)malloc (sizeof(t_pos*) * (y + 1));
     while(y--)
         tat->mappa[y] = (t_pos*)malloc (sizeof(t_pos) * (x + 1));
-}
+}*/
 
-void    ft_img_init(t_vb *tat, t_img *mat)
-{
-    int     i;
-
-    i = 0;
-    mat->img_h = 32;
-    mat->img_w = 32;
-    while(i <= 2)
-    {
-        if(/*coordinate x e y == coordinate di exit*/)
-        {
-            mat->pg = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/BeninoExit.xpm", &mat->img_h, &mat->img_w);
-            break;
-        }
-        else if(i = 0)
-            mat->pg = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/benny.xpm", &mat->img_h, &mat->img_w);
-        else if(i = 1)
-            mat->pg = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/benny1.xpm", &mat->img_h, &mat->img_w);
-        else if(i = 2)
-        {
-            mat->pg = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/benny2.xpm", &mat->img_h, &mat->img_w);
-            i = 0;
-        } 
-        if(keycode == 65361 || keycode == 65363 || keycode == 65362 || keycode 65364)
-            i++;
-    }
-}
-
-void    image_to_win()
-{
-    	mlx_put_image_to_window(t_vb,t_win_list *win,t_img *img, int x,int y)
-}
 
 int     ft_key_handler(int keycode, t_vb *tat)
 {
@@ -106,23 +92,75 @@ int     ft_key_handler(int keycode, t_vb *tat)
         close_win(tat);
     return(0);
 }
+
+void    ft_img_init(t_img *mat, t_vb *tat)
+{
+    mat->img_h = 32;
+    mat->img_w = 32;
+    mat->benino = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/benny.xpm", &mat->img_h, &mat->img_w);
+    mat->ground = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/Floor.xpm", &mat->img_h, &mat->img_w);
+    mat->wall = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/Wall1.xpm", &mat->img_h, &mat->img_w);
+    mat->jesus = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/Jesusticazzi1.xpm", &mat->img_h, &mat->img_w);
+    mat->penguin = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/Penguin.xpm", &mat->img_h, &mat->img_w);
+    mat->door = mlx_xpm_file_to_image(tat->mlx, "/Ninjia&co/Exit.xpm", &mat->img_h, &mat->img_w);
+}
+
+void    image_to_win(t_vb *tat, t_cord cord)
+{
+    if(tat->mappa[cord.y][cord.x] = '1')
+        mlx_put_image_to_window(tat, tat->mlx_win, tat->imag->wall, tat->size.x * 32, tat->size.y * 32);
+    else if(tat->mappa[cord.y][cord.x] = '0')
+        mlx_put_image_to_window(tat, tat->mlx_win, tat->imag->ground, tat->size.x * 32, tat->size.y * 32);
+    else if(tat->mappa[cord.y][cord.x] = 'P')
+        mlx_put_image_to_window(tat, tat->mlx_win, tat->imag->wall, tat->size.x * 32, tat->size.y * 32);
+    else if(tat->mappa[cord.y][cord.x] = 'E')
+        mlx_put_image_to_window(tat, tat->mlx_win, tat->imag->door, tat->size.x * 32, tat->size.y * 32);
+    else if(tat->mappa[cord.y][cord.x] = 'J')
+        mlx_put_image_to_window(tat, tat->mlx_win, tat->imag->jesus, tat->size.x * 32, tat->size.y * 32);
+    else if(tat->mappa[cord.y][cord.x] = 'C')
+        mlx_put_image_to_window(tat, tat->mlx_win, tat->imag->penguin, tat->size.x * 32, tat->size.y * 32);
+}   
+int ft_printmap(char **map, t_vb tat)
+{
+    int y;
+    int x;
+    t_cord pos;
+
+    y = 0;
+    while(map[y])
+    {
+        x = 0;
+        pos.y = y;
+        while(map[y][x])
+        {
+            pos.x = x;
+            image_to_win(&tat, pos);
+            x++;
+        }
+        y++;
+    }
+    return (0);
+}
 int main(int ac, char **av)
 {
     t_vb *tat;
-    t_img *mat;
+    t_img *visual;
+    t_cord  *cord;
     
     if (ac == 2)
     {
     tat = malloc (sizeof (t_vb));
-    mat = malloc (sizeof (t_img));
-    tat->map = getmap(av[1]);
+    visual = malloc (sizeof (t_img));
+    tat->mappa = getmap(av[1]);
     //printf("\n%s\n", tat->map);
     //printf("\n%p\n", tat->mlx_win);
-    map_cord(tat, tat->map);
-    image_to_win(tat, tat->map, tat->mlx_win, );
+    //map_cord(tat, tat->map);
+    tat->size.x = ft_strlen(tat->mappa[0]);
+    tat->size.y = ft_matlen(tat->mappa);
     tat->mlx = mlx_init();
-    tat->mlx_win = mlx_new_window(tat->mlx, tat->coordin.x * SIZE, tat->coordin.y * SIZE, "so_short");
-    ft_img_init(tat, mat);
+    tat->mlx_win = mlx_new_window(tat->mlx, tat->size.x * 32, tat->size.y * 32, "so_long");
+    ft_img_init(tat->imag, tat);
+    ft_printmap(tat->mappa, *tat);
     mlx_hook(tat->mlx_win, 17, 0, close_win, &tat);
     mlx_key_hook(tat->mlx_win, ft_key_handler, &tat);
     mlx_loop(tat->mlx);
--- so_long.h
+++ so_long.h
@@ -1,46 +1,39 @@
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
-#define SIZE 32
+//#define SIZE 32
 
 #include "mlx/mlx.h"
 #include <stdlib.h>
 #include "libft/libft.h"
 #include <fcntl.h>
 
-typedef struct s_pos{
-    char    type;
-    struct  s_pos   *w;
-    struct  s_pos   *a;
-    struct  s_pos   *s;
-    struct  s_pos   *d;
-    char    fload;
-} t_pos;
-
 typedef struct s_cord{
     int     x;
     int     y;
 } t_cord;
 
-
-typedef struct s_dioc{
-    void    *mlx;
-    void    *mlx_win;
-    char    *map;
-    t_cord  coordin;
-    t_pos   **mappa;
-} t_vb;
-
 typedef struct s_img{
-    void    *pg;
+    char    type;
+    void    *benino;
     void    *ground;
     void    *wall;
     void    *door;
     void    *intern_wall;
-    void    *snake;
-    void    *potion;
+    void    *jesus;
+    void    *penguin;
     int     img_w;
     int     img_h;
+    char    fload;
 } t_img;
+
+typedef struct s_vb{
+    void    *mlx;
+    void    *mlx_win;
+ // char    *map;
+    t_cord  size;
+    char  **mappa;
+    t_img   *imag;
+} t_vb;
 
 #endif
